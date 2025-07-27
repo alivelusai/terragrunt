523dcf9011ebfb93cef8a6dd48a6f34fc9796637
alivelusai <alivelusainikhil@gmail.com>
2025-07-27 13:15:14 +0530

    pushing files

---
+++ k8s-demo/envs/dev/.terragrunt-cache/tNUVSK0hnEg8qJbnr59BgN_Wkm0/sVlBkK4LAFPAX83QTrKWahON44U/main.tf
@@ -0,0 +1,12 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "default"  # Change as per your cluster
+}
+
+resource "kubernetes_namespace" "ns" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+variable "namespace" {}
---
+++ k8s-demo/envs/dev/.terragrunt-cache/tNUVSK0hnEg8qJbnr59BgN_Wkm0/sVlBkK4LAFPAX83QTrKWahON44U/terragrunt.hcl
@@ -0,0 +1,7 @@
+terraform {
+  source = "/home/alivelu/k8s-demo/namespace/main.tf"
+}
+
+inputs = {
+  namespace = "dev-namespace"
+}
